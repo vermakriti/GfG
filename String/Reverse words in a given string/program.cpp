2d3b5a5efdb971fb15ca26818e20fad684ef86cb
vermakriti <kriti.verma.it@gmail.com>
2021-08-02 16:09:43 +0530

    updated

--- String/Reverse words in a given string/program.cpp
+++ String/Reverse words in a given string/program.cpp
@@ -1,31 +1,25 @@
-class Solution
-{
+class Solution{
     public:
-    //Function to reverse words in a given string.
     string reverseWords(string S) 
     { 
+        string s="",output="";
         stack<string> st;
-        string s="";
         for(int i=0;i<S.size();i++){
-            if(S[i]!='.'){
-                s+=S[i];
-            }
-            else {
+            if(S[i]=='.'){
                 st.push(s);
+                st.push(".");
                 s.clear();
                 s="";
             }
+            else{
+                s+=S[i];
+            }
         }
         st.push(s);
-        
-        string ans="";
         while(!st.empty()){
-            
-            ans+=st.top();
+            output+=st.top();
             st.pop();
-            ans+='.';
         }
-        ans.pop_back();
-        return ans;
+        return output;
     } 
 };
